f0794f6fadf02b94f9bbc7677adc0bed2e5ae53f
DevNado <oluwatobilobafatoba@gmail.com>
2025-08-03 18:54:04 +0100

    Update jobListings.jsx

--- src/component/jobListings.jsx
+++ src/component/jobListings.jsx
@@ -7,22 +7,25 @@ const JobListings = ({ isHome = false }) => {
   const [jobs, setjobs] = useState([]);
   const [loading, setloading] = useState(true);
 
-  useEffect( () => {
-    const fetchJobs = async () => {
-      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs' 
-      try {
-       const res = await fetch(apiUrl);
+useEffect(() => {
+  const fetchJobs = async () => {
+    try {
+      const res = await fetch('https://api.jsonbin.io/v3/b/688f995ff7e7a370d1f2f39e');
       const data = await res.json();
-      setjobs(data); 
-      }
-      catch (error) {
-        console.error('Error fetching jobs:', error);
-      } finally {
-        setloading(false);
-      }
-    }
 
-    fetchJobs();
+    
+      const jobs = isHome ? data.record.slice(0, 3) : data.record;
+      setjobs(jobs);
+    } catch (error) {
+      console.error('Error fetching jobs:', error);
+    } finally {
+      setloading(false);
+    }
+  };
+
+  fetchJobs();
+}, []);
+
   })
   return (
     <section className="bg-blue-50 px-4 py-10">
